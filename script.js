cb7e1b366c2d6a452365aae9a7839918536929cb
zaddyastro <zaddyastro@gmail.com>
2025-05-17 01:16:49 +0530

    Update script.js

--- script.js
+++ script.js
@@ -11,6 +11,19 @@ const responses = [
   "Power is knowing what you want and going after it.",
   "Don't make me use my Blair stare.",
   "Queen B always wins.",
+  "You’re nobody until you’re talked about.",
+  "Destiny is for losers. It’s just a stupid excuse to wait for things to happen instead of making them happen.",
+  "Whoever said money doesn’t buy happiness didn’t know where to shop.",
+  "You can't make people love you, but you can make them fear you.",
+  "The worst thing you can do is think that you’re not good enough.",
+  "You need to stop thinking about what everyone wants from you and start thinking about what you want from yourself.",
+  "You never really know who your friends are until things get really serious.",
+  "I’m not a stop along the way. I’m a destination.",
+  "Revenge is sweet and not fattening.",
+  "You know I love a good plan, but sometimes you just have to throw caution to the wind.",
+  "I’m not a backup plan, and definitely not your second choice.",
+  "Being bold is better than being boring.",
+  "If you really want something, you don’t stop for anyone or anything until you get it."
 ];
 const blastButton = document.getElementById('askBtn');
 const clickSound = document.getElementById('clickSound');
